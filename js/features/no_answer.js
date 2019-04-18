7a47ce63ffa3ea69d1c9b0f635094ab039fd66b6
Cocagne Anthonin <anthococagne@hotmail.fr>
2019-04-18 18:36:24 +0200

    Rename images folder + Rename icons + Remove unused images

--- js/features/no_answer.js
+++ js/features/no_answer.js
@@ -15,7 +15,7 @@ function showNoAnswerImageOnQuestion() {
     let questionVoteDiv = document.getElementsByClassName("js-voting-container")[questionId];
 
     let noAnswerImg = document.createElement("IMG");
-    noAnswerImg.src = chrome.extension.getURL('/images/worrying-emoji.png');
+    noAnswerImg.src = chrome.extension.getURL('/img/worrying_emoji.png');
     noAnswerImg.width = 40;
 
     questionVoteDiv.appendChild(noAnswerImg);
